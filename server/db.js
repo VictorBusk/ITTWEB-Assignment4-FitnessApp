d042b9a4b9d02369fa2867dd014a60cfaabe40f6
Jeppe Stærk <jeppe@staerk.io>
2017-11-28 13:49:50 +0100

    update

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL, {useMongoClient: true});
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 require('./models/exercise');
 require('./models/workout');
 require('./models/user');
